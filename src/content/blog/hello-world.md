ab84c77e5d421abd9b9ac22b4aa2ddfa2694781f
Tamir <tmelzerb@gmail.com>
2024-01-16 22:00:48 -0800

    stripped away lots of stuff, home page with rendered blog collection

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,7 @@
+---
+title: 'hello world!'
+description: 'a few introductory thoughts for this blog'
+published: 'Jan 16 2024'
+---
+
+testing testing testing
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+  }),
 });
 
 export const collections = { blog };
