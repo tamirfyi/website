66ba887a0e18d8197a369ac8dc7a729db3d8a351
Tamir <tmelzerb@gmail.com>
2024-01-19 17:04:23 -0800

    pretty much done & added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 
 // https://astro.build/config
--- src/content/blog/hello-world.md
+++
@@ -1,7 +0,0 @@
----
-title: 'hello world!'
-description: 'a few introductory thoughts for this blog'
-published: 'Jan 16 2024'
----
-
-testing testing testing
